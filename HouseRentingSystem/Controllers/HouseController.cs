8c8e793acdb345a92e19e0e0d4228184da6969bc
mireyaa8 <miri24508@gmail.com>
2026-03-20 09:39:27 +0200

    Add HouseRentingSystem project

---
+++ HouseRentingSystem/Controllers/HouseController.cs
@@ -0,0 +1,34 @@
+﻿using HouseRentingSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    public class HouseController : Controller
+    {
+        private List<HouseViewModel> houses = new List<HouseViewModel>
+        {
+            new HouseViewModel
+            {
+                Name = "Beach house",
+                Description = "Miami, Florida",
+                ImageUrl = "https://robbreport.com/wp-content/uploads/2017/06/miami-grove-house-backyard-pool.jpg?w=1000"
+            },
+            new HouseViewModel
+            {
+                Name = "House 2",
+                Description = "Description for House 2",
+                ImageUrl = "https://example.com/house2.jpg"
+            },
+            new HouseViewModel
+            {
+                Name = "House 3",
+                Description = "Description for House 3",
+                ImageUrl = "https://example.com/house3.jpg"
+            }
+        };
+        public IActionResult AllHouses()
+        {
+            return View();
+        }
+    }
+}
---
+++ HouseRentingSystem/Models/HouseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSystem.Models
+{
+    public class HouseViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public string ImageUrl { get; set; }
+    }
+}
